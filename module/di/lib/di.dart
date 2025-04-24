b660798a910aea96c0c06fd687cc9de3b6b9f507
이정환/기술지원실/II <cookiegon3434@injeinc.co.kr>
2025-04-24 09:20:51 +0900

    repository, usecase di

--- module/di/lib/di.dart
+++ module/di/lib/di.dart
@@ -1,5 +1,29 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:data/data.dart';
+import 'package:domain/domain.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupDi() {
+  // DB
+  getIt.registerLazySingleton<KDriftDatabase>(() => KDriftDatabase());
+
+  // DAO
+  getIt.registerLazySingleton(() => getIt<KDriftDatabase>().contactDao);
+
+  // Datasource
+  getIt.registerLazySingleton<ContactLocalDatasource>(
+    () => ContactLocalDatasourceImpl(getIt()),
+  );
+
+  // Repository
+  getIt.registerLazySingleton<ContactRepository>(
+    () => ContactRepositoryImpl(getIt()),
+  );
+
+  // usecase
+  getIt.registerLazySingleton(() => GetContactsUseCase(getIt()));
+  getIt.registerLazySingleton(() => CreateContactUseCase(getIt()));
+  getIt.registerLazySingleton(() => UpdateContactUseCase(getIt()));
+  getIt.registerLazySingleton(() => DeleteContactUseCase(getIt()));
 }
